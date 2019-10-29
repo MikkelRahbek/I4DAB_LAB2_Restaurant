60f7759209f24634927cb0730f47aba2febca073
Mikkel Rahbek <Mikkel.rahbek@gmail.com>
2019-10-29 15:17:44 +0100

    Added DB Connection w. MikkelsString Morten skal tilføje sin string

---
+++ I4DAB_LAB2/I4DAB_LAB2/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace I4DAB_LAB2
+{
+
+
+    public class AppDbContext : DbContext
+    {    //private DbSet<Door> doors { get; set; }
+
+        string MikkelsCString = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=I4DAB_LAB2;Data Source=DESKTOP-9NFPK3F";
+        string MortensCString = "23";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(MikkelsCString);
+        }
+    }
+}
